7f9609785e3de57eb701808104bd7639d5cad0ce
AlanLi <64143788+AlanLiZQ6@users.noreply.github.com>
2025-09-19 23:43:06 -0400

    combination

--- src/lfd/heartbeat_client.py
+++ src/lfd/heartbeat_client.py
@@ -1,27 +1,40 @@
 import argparse
 import time
 import requests
+import os
 
-def lfd1(host, port, heartbeat_freq, timeout):
+def log(log_file, text):
+    print(text)
+    """Print and write log to log file."""
+    with open(log_file, "a") as f:
+        f.write(text + "\n")
+        
+def lfd1(host, port, heartbeat_freq, timeout, log_file):
+    fld_id = "LFD1"
+    server_id = "S1"
     last_response_time = time.time()
     server_url = f"http://{host}:{port}"
 
     while True:
         start_time = time.time()
-        print(f"[LFD1] Sending heartbeat to {server_url}/heartbeat")
+        log(log_file, f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] LFD1 Sending heartbeat to {server_id}")
+        # print(f"[{time.strftime("%Y-%m-%d %H:%M:%S")}] LFD1 Sending heartbeat to {server_url}/heartbeat")
         try:
-            r = requests.get(f"{server_url}/heartbeat", timeout=timeout)
+            r = requests.get(f"{server_url}/heartbeat", params={"fld_id": fld_id}, timeout=timeout)
             if r.status_code == 200 and r.json().get("ok"):
                 last_response_time = time.time()
-                print(f"[LFD1] Heartbeat acknowledged by {r.json().get('replica_id')} at {time.ctime()}")
+                log(log_file, f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] LFD1 Heartbeat acknowledged by {r.json().get('replica_id')}")
+                # print(f"[{time.strftime("%Y-%m-%d %H:%M:%S")}] LFD1 Heartbeat acknowledged by {r.json().get('replica_id')} at {time.ctime()}")
             else:
-                print(f"[LFD1 WARN] Unexpected heartbeat response: {r.text}")
-
+                log(log_file, f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] LFD1 WARN: Unexpected heartbeat response: {r.text}")
+                # print(f"[{time.strftime("%Y-%m-%d %H:%M:%S")}] LFD1 WARN: Unexpected heartbeat response: {r.text}")
         except requests.exceptions.RequestException:
             if time.time() - last_response_time > timeout:
-                print(f"[LFD1 ERROR] Heartbeat failed! Server {server_url} not responding.")
+                log(log_file, f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] LFD1 ERROR Heartbeat failed! {server_id} not responding.")
+                # print(f"[{time.strftime("%Y-%m-%d %H:%M:%S")}] LFD1 ERROR Heartbeat failed! Server {server_url} not responding.")
             else:
-                print(f"[LFD1 WARN] Temporary failure, will retry...")
+                log(log_file, f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] LFD1 WARN Temporary failure, will retry...")
+                # print(f"[{time.strftime("%Y-%m-%d %H:%M:%S")}] LFD1 WARN Temporary failure, will retry...")
 
         elapsed = time.time() - start_time
         time.sleep(max(0, heartbeat_freq - elapsed))
@@ -33,6 +46,9 @@ if __name__ == "__main__":
     parser.add_argument("--freq", type=float, default=5.0, help="Heartbeat frequency in seconds")
     parser.add_argument("--timeout", type=float, default=10.0, help="Heartbeat timeout in seconds")
     args = parser.parse_args()
-
-    print(f"[INFO] Starting LFD1 to {args.host}:{args.port} with heartbeat_freq={args.freq}s")
-    lfd1(args.host, args.port, args.freq, args.timeout)
+    start_time_filename  = time.strftime("%Y%m%d_%H:%M:%S")
+    fld_id = 1
+    log_file = os.path.join(os.path.dirname(__file__), "..",'..', "logs", f"fld_{fld_id}_log_{start_time_filename}.txt")
+    log(log_file, f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] Starting LFD1 to {args.host}:{args.port} with heartbeat_freq={args.freq}")
+    # print(f"[{time.strftime("%Y-%m-%d %H:%M:%S")}] Starting LFD1 to {args.host}:{args.port} with heartbeat_freq={args.freq}s")
+    lfd1(args.host, args.port, args.freq, args.timeout, log_file)
